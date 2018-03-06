747dd2aa730144bcd08ae4e6a9bbabf6ba2d3816
Johan Faerch <jf@keeptrack.nu>
2018-03-06 14:18:58 +0100

    methods updated

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,23 +1,43 @@
 class BookingsController < ApplicationController
 
   def index
+    @bookings = Booking.all
   end
 
   def show
+    @booking = Booking.find(params[:id])
   end
 
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    if @booking.save(booking_params)
+      redirect_to bookings_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @booking = Booking.find(booking_params)
   end
 
   def update
+    @booking = Booking.find(booking_params)
+    @task.update(booking_params)
+    redirect_to bookings_path
   end
 
   def destroy
   end
+
+  private
+
+  def booking_params
+    params.require(:booking)
+  end
+
 end
