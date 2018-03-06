2f422db1863813b6b663c62e84d64a10f35c7ee6
Tanja Kohlen <tanja.kohlen@gmail.com>
2018-03-06 12:14:23 +0100

    add methods index, show, new, create and params method  in spaces controller

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,2 +1,29 @@
 class SpacesController < ApplicationController
+
+  def index
+    @spaces = Space.all
+  end
+
+  def show
+    @spaces = Space.find(params[:id])
+  end
+
+  def new
+    @spaces = Space.new
+  end
+
+  def create
+    @spaces = Space.new(spaces_params)
+    space.save
+    redirect_to space_path(@spaces)
+  end
+
+  private
+
+  def space_params
+    params.require(:space).permit(:description)
+  end
+
 end
+
+
