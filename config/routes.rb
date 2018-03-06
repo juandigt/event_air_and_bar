80a61f9f74f384c45079b509c1d3961b83b85e7b
juan diego garcia <juandigt@hotmail.com>
2018-03-06 12:03:17 +0100

    add the routes for bookings and spaces

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'bookings/index'
 
-  get 'bookings/create'
-
+  resources :spaces
+  resources :bookings
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
