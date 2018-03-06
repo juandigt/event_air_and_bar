51dbabf2ed1e5a6d21348e7db46b089d781ffad3
juan diego garcia <juandigt@hotmail.com>
2018-03-06 18:13:55 +0100

    conflict resalut

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -15,8 +15,7 @@ class SpacesController < ApplicationController
 
   def create
     @space = Space.new(space_params)
-     @space.user = current_user
-    @space.save
+    @space.user = current_user
     if @space.save
       redirect_to space_path(@space)
     else
